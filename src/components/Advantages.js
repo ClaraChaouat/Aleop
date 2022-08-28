f23a4edd44e111a20226f8a26457ff4cac6f7bca
Clara CHAOUAT <chaouatclara337@hotmail.com>
2022-08-28 20:29:17 +0300

    Removed unused icon

--- src/components/Advantages.js
+++ src/components/Advantages.js
@@ -1,14 +1,11 @@
 import "./advantages.scss";
-import returnIcon from "../images/icon-return-left.svg";
+
 
 export function Advantages() {
-
-
   return (
     <div className="advantagesContainer">
       <div className="advantagesTitle">
         <p>Advantages*</p>
-       
       </div>
     </div>
   );
